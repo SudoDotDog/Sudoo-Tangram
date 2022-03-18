7e3d479aa4c0e1c209f4438cdb83d5eb619dbd14
WMXPY <wm@mengw.io>
2022-03-17 19:03:21 -0500

    add lsice controller

--- src/tangram/declare.ts
+++ src/tangram/declare.ts
@@ -4,8 +4,73 @@
  * @description Declare
  */
 
+import * as React from "react";
+
 export type PolygonPoint = {
 
     readonly x: number;
     readonly y: number;
+};
+
+export type PolygonTransform = {
+
+    readonly xOffset: number;
+    readonly yOffset: number;
+
+    readonly rotation: number;
+};
+
+export type PolygonFigure = {
+
+    readonly color: string;
+    readonly strokeColor?: string;
+};
+
+export type PolygonControl = {
+
+    readonly onClick?: React.MouseEventHandler<SVGPolygonElement>;
+    readonly onDrag?: React.MouseEventHandler<SVGPolygonElement>;
+
+    readonly onMouseEnter?: React.MouseEventHandler<SVGPolygonElement>;
+    readonly onMouseLeave?: React.MouseEventHandler<SVGPolygonElement>;
+
+    readonly onMouseDown?: React.MouseEventHandler<SVGPolygonElement>;
+    readonly onMouseUp?: React.MouseEventHandler<SVGPolygonElement>;
+    readonly onMouseMove?: React.
+    MouseEventHandler<SVGPolygonElement>;
+
+    readonly onScroll?: React.WheelEventHandler<SVGPolygonElement>;
+};
+
+export type TangramTransform = {
+
+    readonly bigLeft: PolygonTransform;
+    readonly bigTop: PolygonTransform;
+    readonly smallBottom: PolygonTransform;
+    readonly square: PolygonTransform;
+    readonly smallCenter: PolygonTransform;
+    readonly parallelogram: PolygonTransform;
+    readonly medium: PolygonTransform;
+};
+
+export type TangramFigure = {
+
+    readonly bigLeft: PolygonFigure;
+    readonly bigTop: PolygonFigure;
+    readonly smallBottom: PolygonFigure;
+    readonly square: PolygonFigure;
+    readonly smallCenter: PolygonFigure;
+    readonly parallelogram: PolygonFigure;
+    readonly medium: PolygonFigure;
+};
+
+export type TangramControl = {
+
+    readonly bigLeft?: PolygonControl;
+    readonly bigTop?: PolygonControl;
+    readonly smallBottom?: PolygonControl;
+    readonly square?: PolygonControl;
+    readonly smallCenter?: PolygonControl;
+    readonly parallelogram?: PolygonControl;
+    readonly medium?: PolygonControl;
 };
--- src/tangram/slice.tsx
+++ src/tangram/slice.tsx
@@ -5,35 +5,71 @@
  */
 
 import * as React from "react";
-import { PolygonPoint } from "./declare";
+import { PolygonControl, PolygonFigure, PolygonPoint, PolygonTransform } from "./declare";
 
 export type TangramSliceProps = {
 
-    readonly color: string;
-    readonly points: PolygonPoint[];
+    readonly transform: PolygonTransform;
+    readonly figure: PolygonFigure;
+    readonly control?: PolygonControl;
 
-    readonly strokeColor?: string;
+    readonly points: PolygonPoint[];
 };
 
 export const TangramSlice: React.FC<TangramSliceProps> = (props: TangramSliceProps) => {
 
-    const pointsString: string = props.points.map((point: PolygonPoint) => {
-        return `${point.x},${point.y}`;
-    }).join(" ");
+    const pointsString: string = props.points
+        .map((point: PolygonPoint) => {
+            return `${point.x},${point.y}`;
+        })
+        .join(" ");
+
+    const transformList: string[] = [];
+
+    if (typeof props.transform.xOffset === 'number') {
+        transformList.push(`translateX(${props.transform.xOffset}px)`);
+    }
+
+    if (typeof props.transform.yOffset === 'number') {
+        transformList.push(`translateY(${props.transform.yOffset}px)`);
+    }
+
+    if (typeof props.transform.rotation === 'number') {
+        transformList.push(`rotate(${props.transform.rotation}deg)`);
+    }
 
     const style: React.CSSProperties = {
-        fill: props.color,
+
+        fill: props.figure.color,
+        transform: transformList.join(" "),
     };
 
-    if (props.strokeColor) {
-        style.stroke = props.strokeColor;
+    if (props.figure.strokeColor) {
+
+        style.stroke = props.figure.strokeColor;
         style.strokeWidth = "0.5";
     }
+
+    const fixedControl: PolygonControl = {
+        ...props.control,
+    };
 
     return (<polygon
         points={pointsString}
         strokeLinecap="round"
         strokeLinejoin="round"
         style={style}
+
+        onMouseEnter={fixedControl.onMouseEnter}
+        onMouseLeave={fixedControl.onMouseLeave}
+
+        onClick={fixedControl.onClick}
+        onDrag={fixedControl.onDrag}
+
+        onMouseDown={fixedControl.onMouseDown}
+        onMouseUp={fixedControl.onMouseUp}
+        onMouseMove={fixedControl.onMouseMove}
+
+        onScroll={fixedControl.onScroll}
     />);
 };
