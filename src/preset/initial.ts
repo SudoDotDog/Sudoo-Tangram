8d4aad5481b58141ca5ed7342d74d1d855556e4f
WMXPY <wm@mengw.io>
2022-03-17 19:03:09 -0500

    add initial preset

---
+++ src/preset/initial.ts
@@ -0,0 +1,73 @@
+/**
+ * @author WMXPY
+ * @namespace Preset
+ * @description Initial
+ */
+
+import { TangramFigure, TangramTransform } from "../tangram/declare";
+
+export const tangramInitialTransformPreset: TangramTransform = {
+
+    bigLeft: {
+        xOffset: 0,
+        yOffset: 0,
+        rotation: 0,
+    },
+    bigTop: {
+        xOffset: 0,
+        yOffset: 0,
+        rotation: 0,
+    },
+    smallBottom: {
+        xOffset: 0,
+        yOffset: 0,
+        rotation: 0,
+    },
+    square: {
+        xOffset: 0,
+        yOffset: 0,
+        rotation: 0,
+    },
+    smallCenter: {
+        xOffset: 0,
+        yOffset: 0,
+        rotation: 0,
+    },
+    parallelogram: {
+        xOffset: 0,
+        yOffset: 0,
+        rotation: 0,
+    },
+    medium: {
+        xOffset: 0,
+        yOffset: 0,
+        rotation: 0,
+    },
+};
+
+export const tangramInitialFigurePreset: TangramFigure = {
+
+    bigLeft: {
+        color: 'red',
+    },
+    bigTop: {
+        color: 'blue',
+    },
+    smallBottom: {
+        color: 'purple',
+    },
+    square: {
+        color: 'cyan',
+    },
+    smallCenter: {
+        color: 'green',
+    },
+    parallelogram: {
+        color: 'yellow',
+    },
+    medium: {
+        color: 'orange',
+    },
+};
+
+
